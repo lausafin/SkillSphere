ccbf0dfa9a80d1436d7eaaa65bf057826a2ab7ec
Lau Safin <laux0523@gmail.com>
2025-10-26 15:43:13 +0100

    remove: old backend

--- backend/src/auth/guards/jwt-auth.guard.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
-// It automatically uses the JwtStrategy by default because of the 'jwt' key
--- backend/src/categories/dto/create-category.dto.ts
+++
@@ -1,9 +0,0 @@
-// src/categories/dto/create-category.dto.ts
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
-
-export class CreateCategoryDto {
-  @IsString({ message: 'Category name must be a string.' })
-  @IsNotEmpty({ message: 'Category name should not be empty.' })
-  @MaxLength(100, { message: 'Category name cannot be longer than 100 characters.' })
-  name: string;
-}
